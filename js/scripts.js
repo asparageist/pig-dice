6f641bd329d7215203d5edcfc40e3fa742a75cdb
Joseph Murray <josephwmurray@gmail.com>
2023-02-21 17:45:58 -0800

    accomplish TDD task, adding dice scores

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 let dice1 = 1;
 let dice2 = 1;
+let score1 = 0;
+let score2 = 0;
 
 document.getElementById("roll-button").addEventListener("click", rollDice);
 
@@ -11,9 +13,17 @@ function rollDice() {
   dice1 = Math.floor(Math.random() * 6) + 1;
   dice2 = Math.floor(Math.random() * 6) + 1;
   displayDice();
+  addScore();
 }
 
 function displayDice() {
   document.querySelector("span#dice1").innerText = dice1;
   document.querySelector("span#dice2").innerText = dice2;
+}
+
+function addScore() {
+  score1 += dice1 + dice2;
+  score2 += dice1 + dice2;
+  document.querySelector("span#score1").innerText = score1;
+  document.querySelector("span#score2").innerText = score2;
 }
--- readme.md
+++ readme.md
@@ -7,3 +7,7 @@ Expected Output: "4"
 Test: "it should display numbers on the front end"
 Code: "innertext.dice1"
 Expected Output: "4"
+
+Test: "it should add the dice to the scores"
+Code: "score1 = dice1 + dice2"
+Expected Output: "8"
