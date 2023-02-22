88e50f8f0eb57324b1e7a45f17a54f16dcf4faba
Joseph Murray <josephwmurray@gmail.com>
2023-02-21 19:21:33 -0800

    accomplish TDD test, achieving a game over through scoring 100

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,33 @@ let dice2 = 1;
 let score1 = 0;
 let score2 = 0;
 let player = 0;
+let gameOver = false;
 
-document.getElementById("roll-button").addEventListener("click", rollDice);
+document.getElementById("roll-button").addEventListener("click", gameOn);
+
+window.onload = function() {
+  document.getElementById("roll-button").removeAttribute("class", "hidden");
+}
+
+function gameOn() {
+  if (gameOver === false) {
+    rollDice();
+  } else {
+    gameOff();
+  }
+}
+
+function gameOff() {
+  gameOver = true;
+  document.getElementById("reset").removeAttribute("class", "hidden");
+  document.getElementById("roll-button").setAttribute("class", "hidden");
+}
 
 function reset() {
   document.getElementById("bust").setAttribute("class", "hidden");
+  document.getElementById("reset").setAttribute("class", "hidden");
   player = 0;
+  gameOver = false;
 }
 
 function rollDice() {
@@ -16,7 +37,6 @@ function rollDice() {
   dice2 = Math.floor(Math.random() * 6) + 1;
   displayDice();
   addScore();
-  setPlayer();
 }
 
 function displayDice() {
@@ -24,12 +44,31 @@ function displayDice() {
   document.querySelector("span#dice2").innerText = dice2;
 }
 
+function isGameOn() {
+  if (score1 < 100 && score2 < 100) {
+    setPlayer();
+  } else {
+    if (score1 >= 100) {
+      document.querySelector("span#score1").innerText = (score1 + " WINNER!");
+      gameOff();
+    } else if (score2 >= 100) {
+      document.querySelector("span#score2").innerText = (score2 + " WINNER!");
+      gameOff();
+    }
+  }
+}
+
 function addScore() {
-  score1 += dice1 + dice2;
-  score2 += dice1 + dice2;
+  if (player === 0) {
+    score1 += dice1 + dice2;
+  } else {
+    score2 += dice1 + dice2;
+  }
   document.querySelector("span#score1").innerText = score1;
   document.querySelector("span#score2").innerText = score2;
-}
+  isGameOn();
+} 
+
 
 function setPlayer() {
   if (player === 0) {
--- readme.md
+++ readme.md
@@ -15,3 +15,11 @@ Expected Output: "8"
 Test: "it should alternate between players"
 Code: "if player = 0, + 1; else, = 0;"
 Expected Output: "Player 1"
+
+Test: "it should only add score to appropriate player"
+Code: "if player = 0, score1 = dice1 + dice2"
+Expected Output: "Player 1 score = 8"
+
+Test: "it should end the game if a player reaches 100"
+Code: "if score >= 100, addScore()"
+Expected Output: "Player 1 Score = 100, Player 1 wins!"
